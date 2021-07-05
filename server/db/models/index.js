572e8bc1ce746b378cccd5999581eb31309ffe3d
Jason Chen <j.chen2303@gmail.com>
2021-07-05 01:57:12 -0400

    imported and exported items model on index page

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,11 +1,14 @@
-const db = require("../index");
-const Sequelize = require("sequelize");
+const db = require('../index');
+const Sequelize = require('sequelize');
 
-const User = require("./users");
+const User = require('./users');
+
+const Item = require('./items');
 
 //associations
 
 module.exports = {
   db,
   User,
+  Item,
 };
