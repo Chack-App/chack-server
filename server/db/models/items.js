120d78647009c083cf610d769c996cf9b2844555
Jason Chen <j.chen2303@gmail.com>
2021-07-05 01:56:47 -0400

    added attributes to items model

--- server/db/models/items.js
+++ server/db/models/items.js
@@ -1,2 +1,23 @@
 const Sequelize = require('sequelize');
 const { db } = require('../index');
+
+const Item = db.define('items', {
+  name: {
+    type: Sequelize.STRING,
+  },
+  price: {
+    type: Sequelize.INTEGER,
+  },
+  isClaimed: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+  splitBetween: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 1,
+  },
+});
+
+module.exports = Item;
