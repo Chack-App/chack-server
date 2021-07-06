a66aa881eafe075219e5930d22a7efc75192408e
Jason Chen <j.chen2303@gmail.com>
2021-07-06 17:25:51 -0400

    created query for events specific to user Id to use to load after sign-in

--- schema/schema_users.js
+++ schema/schema_users.js
@@ -1,6 +1,8 @@
-const graphql = require("graphql");
-const { User } = require("../server/db");
-const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLList } = graphql;
+const graphql = require("graphql")
+const { Event } = require("../server/db")
+const { User } = require("../server/db")
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLList } = graphql
+let { EventType } = require("./schema_events")
 
 const UserSchema = new GraphQLObjectType({
   name: "User",
@@ -9,22 +11,44 @@ const UserSchema = new GraphQLObjectType({
     username: { type: GraphQLString },
     password: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    lastName: { type: GraphQLString },
-  }),
-});
+    lastName: { type: GraphQLString }
+  })
+})
 
 //Query
 const user = {
   type: UserSchema,
   args: { id: { type: GraphQLID } },
   resolve(parent, args) {
-    return User.findByPk(args.id);
-  },
-};
+    return User.findByPk(args.id)
+  }
+}
+
+const userEvents = {
+  type: new GraphQLList(EventType),
+  args: { id: { type: GraphQLID } },
+  async resolve(parent, args) {
+    let userEventArr = await User.findOne({
+      where: {
+        id: args.id
+      },
+      include: [
+        {
+          model: Event
+          // include: [User]
+        }
+      ]
+    })
+
+    console.log(userEventArr.events)
+    return userEventArr.events
+  }
+}
 
 module.exports = {
   userQueries: {
     user,
+    userEvents
   },
-  UserSchema,
-};
+  UserSchema
+}
