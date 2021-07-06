40023efd06b226f6a5500c5f71def9581a09fd4d
Cody Swithenbank <80492369+cswithen@users.noreply.github.com>
2021-07-06 07:30:44 -0700

    Update index.js

--- schema/index.js
+++ schema/index.js
@@ -8,11 +8,11 @@ const { GraphQLObjectType, GraphQLSchema } = graphql;
 const MasterQuery = new GraphQLObjectType({
   name: "MasterQuerySchema",
   description: 'This is encompases all Queries',
-  fields: () => ({
+  fields: {
     ...userQueries,
     ...eventQueries
   }
-)});
+});
 
 module.exports = new GraphQLSchema({
   query: MasterQuery,
