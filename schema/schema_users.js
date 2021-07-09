35eac235dcbd4dbbafe97c0556f5367682673b5b
Cody Swithenbank <cody.swithenbank@gmail.com>
2021-07-09 09:29:35 -0700

    added additional parameters to be returned via signup schema/mutation

--- schema/schema_users.js
+++ schema/schema_users.js
@@ -5,7 +5,7 @@ const {
   GraphQLString,
   GraphQLID,
   GraphQLList,
-  GraphQLNonNull,
+  GraphQLNonNull
 } = graphql
 
 let { EventType } = require("./schema_events")
@@ -127,7 +127,9 @@ const signup = {
   type: AuthType,
   args: {
     email: { type: GraphQLString },
-    password: { type: GraphQLString }
+    password: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    lastName: { type: GraphQLString }
   },
   async resolve(parent, args) {
     const user = await User.findOne({
@@ -137,15 +139,17 @@ const signup = {
     if (user) {
       throw new Error("This user already exists")
     }
-    await User.create({
+    const { email, firstName, lastName, id } = await User.create({
       email: args.email,
-      password: args.password
+      password: args.password,
+      firstName: args.firstName,
+      lastName: args.lastName
     })
     const token = await User.authenticate({
       email: args.email,
       password: args.password
     })
-    return { token }
+    return { token, email, firstName, lastName, id }
   }
 }
 
